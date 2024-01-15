870b513f30b2aad28f32fb7ed1cb14a19418da34
Hung Do <hung.do@workbuddy.com>
2024-01-15 21:26:49 +0700

    Variable learning

---
+++ nodejs/lesson_01/VariableLearning.js
@@ -0,0 +1,20 @@
+let myName = 'Hung';
+let myAge = 20;
+
+console.log(myName);
+console.log(myAge);
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+let isTrue = true;
+
+console.log(isTrue);
+
+console.log(typeof myName);
+console.log(typeof myAge);
+console.log(typeof isTrue);
+
+const PI = 3.14;
+console.log(PI);
+
