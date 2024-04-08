7a6d09634e0a2426b0eca2b5a1827af980930c75
Hung Do <hung.do@workbuddy.com>
2024-04-08 20:44:18 +0700

    Day5-SwitchCase statement

--- nodejs/lessons/day_06/SwitchCaseStatement.js
+++ nodejs/lessons/day_06/SwitchCaseStatement.js
@@ -1,1 +1,33 @@
+import { readLine } from "readline-sync";
 const inputNum = getUserInputNum();
+
+switch (inputNum) {
+  case 2:
+    console.log("Monday");
+    break;
+  case 3:
+    console.log("Tuesday");
+    break;
+  case 4:
+    console.log("Wednesday");
+    break;
+  case 5:
+    console.log("Thursday");
+    break;
+  case 6:
+    console.log("Friday");
+    break;
+  case 7:
+    console.log("Saturday");
+    break;
+  case 8:
+    console.log("Sunday");
+    break;
+  default:
+    console.log("Invalid input");
+    break;
+}
+
+function getUserInputNum() {
+  return readLine.question("Enter a number between 2 and 8: ");
+}
