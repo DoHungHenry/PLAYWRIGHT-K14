b240a90ccfda5fe5876e31410478713ac8ba3c5c
Hung Do <hung.do@workbuddy.com>
2024-04-10 21:23:21 +0700

    Nodejs day5- break and continue to control loop

---
+++ nodejs/lessons/day_05/LoopFlowControlKeyword.js
@@ -0,0 +1,11 @@
+for (let index = 0; index < 5; index++) {
+    if (number === 0) {
+        break; // Exit the loop
+    }
+}
+
+for (let index = 0; index < 5; index++) {
+    if (number === 0) {
+        continue; // Skip the current iteration
+    }
+}
