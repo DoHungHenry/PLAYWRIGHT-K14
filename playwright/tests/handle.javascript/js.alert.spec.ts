47054665216316fe7aa01ccb7e1dcfba4d11904f
Hung Do <hung.do@workbuddy.com>
2024-04-11 07:36:26 +0700

    PW day 21 - handle js alert

---
+++ playwright/tests/handle.javascript/js.alert.spec.ts
@@ -0,0 +1,22 @@
+import { test } from '@playwright/test';
+
+const url = 'https://the-internet.herokuapp.com/javascript_alerts';
+
+test('Handle JS Alert', async ({ page }) => {
+    await page.goto(url);
+    const jsAlertLct =  page.locator('button[onclick="jsAlert()"]');
+
+    // MUST define the event first
+    page.on('dialog', async dialog => {
+        await dialog.accept();
+    });
+
+    // Trigger the js alert
+    await jsAlertLct.click();
+    
+
+
+    // wait
+    await page.waitForTimeout(3000);
+
+});
