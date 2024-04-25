0849cf746944eac834e677acbdc1cf7cb7cc3da5
Hung Do <hung.do@workbuddy.com>
2024-04-25 07:51:08 +0700

    nodejs-day 8

---
+++ nodejs/lessons/asynchronous/AsyncAndCallBack.js
@@ -0,0 +1,17 @@
+/* 
+Send api request
+Process response
+Verify response
+Do some work
+*/
+
+const fetch = require('node-fetch');
+
+const url = 'https://jsonplaceholder.typicode.com/posts/1';
+
+
+function sendReq(url) {};
+
+function processResponse(response) {};
+
+function verifyResponse(response) {};
---
+++ nodejs/lessons/asynchronous/AsyncFuntion.js
@@ -0,0 +1,17 @@
+console.log('this is first line');
+
+const argument = 'this is second line';
+
+setTimeout(secondLine(argument), 2000);
+
+console.log('this is third line');
+
+function secondLine(argument) {
+    console.log(this.ten); // bonus part
+    console.log(argument);
+};
+
+// bonus: modify secondline function by bind method
+
+setTimeout(secondLine.bind({ten: 'Teo'}, argument), 2000);
+
